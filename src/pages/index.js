863479f10ddef287a1e23902e025a2daed3909c7
ryanmotamen <ryanmotamen@gmail.com>
2023-04-12 17:16:05 -0700

    adding footer for team members

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,89 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures/FeaturesComponent';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures/FeaturesComponent";
 
-import styles from './index.module.css';
+import { FaApple } from "react-icons/fa";
+import { FcLinux } from "react-icons/fc"
+import { AiFillWindows, AiFillGithub, AiFillLinkedin } from "react-icons/ai"
+import { IconContext } from "react-icons";
+
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-          className={clsx('button button--secondary button--lg', styles.margin)}
+          <a
+            className={styles.downloadButton}
+            href="https://github.com/oslabs-beta/AthenaJS/releases/download/v1.0.0/athena_1.0.0_Mac.dmg"
+            download=""
           >
-            Download
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Getting Started
-          </Link>
+            <FaApple />
+          </a>
+          <a
+            className={styles.downloadButton}
+            href="https://github.com/oslabs-beta/AthenaJS/releases/download/v1.0.0/athena_1.0.0_Win.exe"
+            download=""
+          >
+            <AiFillWindows />
+          </a>
+          <a
+            className={styles.downloadButton}
+            href="https://github.com/oslabs-beta/AthenaJS/releases/download/v1.0.0/athena_1.0.0_Linux.AppImage"
+            download=""
+          >
+            <FcLinux />
+          </a>
         </div>
       </div>
     </header>
   );
 }
 
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+const HomepageFooter = () => {
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      description="Description will go into a meta tag in <head />">
-      <main>
-        <HomepageHeader />
-        <HomepageFeatures />
-      </main>
+    <footer className='fullViewHeightFooter'>
+      AthenaJS Core Team
+      <div className='teamList'>
+        <div className='teamMember'><div>Daniel Chang:</div><div><a href = '#'><AiFillGithub /></a><a href = '#'><AiFillLinkedin /></a></div></div>
+        <div className='teamMember'><span>Christopher Long:</span><a href = '#'> <AiFillGithub /></a> <span> | </span> <a href = '#'> <AiFillLinkedin /></a></div>
+        <div className='teamMember'><span>Derrick Oh:</span><a href = 'https://github.com/Igniphis'> <AiFillGithub /></a> <span> | </span> <a href = 'https://www.linkedin.com/in/derrick-oh-37a84a182/'> <AiFillLinkedin /></a></div>
+        <div className='teamMember'><span>Matthew Fukudome:</span><a href = 'https://github.com/cvalong'><AiFillGithub /></a> <span> | </span> <a href = 'https://www.linkedin.com/in/cvalong/'> <AiFillLinkedin /></a></div>
+        <div className='teamMember'>
+          <span>Ryan Motamen:</span>
+          <a href = 'https://github.com/ryanmotamen'> <AiFillGithub /></a> 
+          <span> | </span> 
+          <a href ='https://www.linkedin.com/in/ryan-motamen/' > <AiFillLinkedin /></a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+
+  const iconStyles = {
+    size: "2rem",
+  };
+
+  return (
+    <Layout description="Description will go into a meta tag in <head />">
+      <IconContext.Provider value={iconStyles}>
+        <main>
+          <HomepageHeader />
+          <HomepageFeatures />
+          <HomepageFooter />
+        </main>
+      </IconContext.Provider>
     </Layout>
   );
 }
